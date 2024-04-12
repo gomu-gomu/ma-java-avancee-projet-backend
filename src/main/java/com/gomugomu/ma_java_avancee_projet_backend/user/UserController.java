35db04f067f40faf4dfd062126f76c8d9606fd0e
eoussama <oussama.essamadi@gmail.com>
2024-04-12 15:34:53 +0000

    added user pagination

--- src/main/java/com/gomugomu/ma_java_avancee_projet_backend/user/UserController.java
+++ src/main/java/com/gomugomu/ma_java_avancee_projet_backend/user/UserController.java
@@ -8,9 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.data.domain.Page;
 
 @RestController
 @RequestMapping("/user")
@@ -29,5 +33,11 @@ public class UserController {
   public ResponseEntity<User> createUser(@RequestBody User user) {
     User createdUser = userService.createUser(user);
     return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+  }
+
+  @GetMapping("/page/{page}")
+  public ResponseEntity<Page<User>> getAllUsersPage(@PathVariable Integer page) {
+    Page<User> users = userService.getAllUsers(page);
+    return ResponseEntity.ok(users);
   }
 }
--- src/main/java/com/gomugomu/ma_java_avancee_projet_backend/user/UserService.java
+++ src/main/java/com/gomugomu/ma_java_avancee_projet_backend/user/UserService.java
@@ -2,17 +2,28 @@ package com.gomugomu.ma_java_avancee_projet_backend.user;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service
 public class UserService {
+
+  @Value("${config.pagination.size}")
+  private byte paginationSize;
 
   @Autowired
   private UserRepository userRepository;
 
   public List<User> getAllUsers() {
     return userRepository.findAll();
+  }
+
+  public Page<User> getAllUsers(Integer page) {
+    return userRepository.findAll(PageRequest.of(page, this.paginationSize));
   }
 
   public User createUser(User user) {
