2e7134e1185102db40d23928b4c163cbdca5e3f2
eoussama <oussama.essamadi@gmail.com>
2024-04-21 23:20:50 +0100

    user details

--- src/main/java/com/gomugomu/ma_java_avancee_projet_backend/user/User.java
+++ src/main/java/com/gomugomu/ma_java_avancee_projet_backend/user/User.java
@@ -1,15 +1,22 @@
 package com.gomugomu.ma_java_avancee_projet_backend.user;
 
+import com.gomugomu.ma_java_avancee_projet_backend.parent.Parent;
+import com.gomugomu.ma_java_avancee_projet_backend.student.Student;
+import com.gomugomu.ma_java_avancee_projet_backend.teacher.Teacher;
+import com.gomugomu.ma_java_avancee_projet_backend.common.BasePrimary;
+
 import lombok.Data;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
-import com.gomugomu.ma_java_avancee_projet_backend.parent.Parent;
-import com.gomugomu.ma_java_avancee_projet_backend.student.Student;
-import com.gomugomu.ma_java_avancee_projet_backend.teacher.Teacher;
-import com.gomugomu.ma_java_avancee_projet_backend.common.BasePrimary;
+import java.util.List;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
@@ -26,7 +33,7 @@ import jakarta.persistence.CascadeType;
 @AllArgsConstructor
 @Table(name = "users")
 @EqualsAndHashCode(callSuper = false)
-public class User extends BasePrimary {
+public class User extends BasePrimary implements UserDetails {
 
     @Column(nullable = false)
     private String email;
@@ -46,4 +53,34 @@ public class User extends BasePrimary {
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     private Teacher teacher;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.type.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/gomugomu/ma_java_avancee_projet_backend/user/UserRepository.java
+++ src/main/java/com/gomugomu/ma_java_avancee_projet_backend/user/UserRepository.java
@@ -2,6 +2,7 @@ package com.gomugomu.ma_java_avancee_projet_backend.user;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +15,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
+
+  Optional<User> findByEmail(String email);
 
   @RestResource(path = "by-all", rel = "by-all")
   @Query("SELECT u FROM User u WHERE (:email IS NULL OR :email = '' OR LOWER(u.email) LIKE LOWER(CONCAT('%', :email, '%'))) AND (COALESCE(:types) IS NULL OR u.type IN :types)")
