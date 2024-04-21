3fc183c30aeb6a687a5682df8270b54b47227719
eoussama <oussama.essamadi@gmail.com>
2024-04-21 23:28:00 +0100

    security and app config

--- src/main/java/com/gomugomu/ma_java_avancee_projet_backend/security/SecurityConfig.java
+++ src/main/java/com/gomugomu/ma_java_avancee_projet_backend/security/SecurityConfig.java
@@ -1,52 +1,45 @@
 package com.gomugomu.ma_java_avancee_projet_backend.security;
 
-import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.http.HttpMethod;
 import org.springframework.security.web.SecurityFilterChain;
-
-import com.gomugomu.ma_java_avancee_projet_backend.user.UserType;
-
-import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+  private final JwtAuthenticationFilter jwtAuthFilter;
+  private final AuthenticationProvider authenticationProvider;
+  private static final String[] WHITE_LIST_URL = {
+      "/api/auth/**",
+      "/v3/api-docs",
+      "/swagger-ui.html"
+  };
 
   @Bean
-  public UserDetailsManager detailsManager(DataSource dataSource, PasswordEncoder noOpPasswordEncoder) {
-    JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-
-    jdbcUserDetailsManager.setUsersByUsernameQuery("select email, password, 1 as enabled from users where email=?");
-    jdbcUserDetailsManager
-        .setAuthoritiesByUsernameQuery("select email, concat('ROLE_', type), 1 as enabled from users where email=?");
-
-    return jdbcUserDetailsManager;
-  }
-
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(configurer -> configurer
-        .requestMatchers(HttpMethod.POST, "/api/auth/**")
-        .hasAnyRole(UserType.ADMIN.getRole(), UserType.TEACHER.getRole(), UserType.PARENT.getRole(),
-            UserType.STUDENT.getRole())
-        .anyRequest().hasRole(UserType.ADMIN.getRole()));
-
-    http.httpBasic(Customizer.withDefaults());
-    http.csrf(csrf -> csrf.disable());
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(req -> req.requestMatchers(WHITE_LIST_URL)
+            .permitAll()
+            .anyRequest()
+            .authenticated())
+        .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+        .authenticationProvider(authenticationProvider)
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
